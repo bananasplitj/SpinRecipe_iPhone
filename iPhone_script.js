d14bba961ca121d3aa574a57f04a7dfe45fd73e2
bananasplitj <kimj1152@newschool.edu>
2024-11-15 02:38:54 -0500

    Update iPhone_script.js

--- iPhone_script.js
+++ iPhone_script.js
@@ -2,3 +2,15 @@
 function toggleBold(element) {
   element.classList.toggle("bold"); // 클래스 추가/제거
 }
+
+// 팝업 열기 함수
+function showPopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "flex"; // 팝업 표시
+}
+
+// 팝업 닫기 함수
+function closePopup() {
+  const popup = document.getElementById("popup");
+  popup.style.display = "none"; // 팝업 숨기기
+}
